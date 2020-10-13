a12135b69d332c4a9c08232858b70a038d026080
pgwilliams <petergwilliams@hotmail.com>
2020-10-13 14:58:03 +0100

    CDI-79 Quieten down curl statements

--- daily-conversion-build/go.sh
+++ daily-conversion-build/go.sh
@@ -29,54 +29,52 @@ release_url=https:///${envPrefix}release.ihtsdotools.org
 
 loginToIMS() {
 	echo "Logging in as $username to $ims_url"
-	curl -i -v --cookie-jar cookies.txt -H 'Accept: application/json' -H "Content-Type: application/json" ${ims_url}/api/authenticate --data '{"login":"'${username}'","password":"'${password}'","rememberMe":"false"}'
+	curl --cookie-jar cookies.txt -H 'Accept: application/json' -H "Content-Type: application/json" ${ims_url}/api/authenticate --data '{"login":"'${username}'","password":"'${password}'","rememberMe":"false"}'
 	echo "Cookies saved"
 }
 
 downloadDelta() {
-curl -sSiv ${tsUrl}/snowstorm/snomed-ct/exports \
+curl -sSi ${tsUrl}/snowstorm/snomed-ct/exports \
   -H 'Connection: keep-alive' \
   -H 'Accept: application/json' \
   -H 'Content-Type: application/json' \
   --cookie cookies.txt \
   --data-binary $'{ "branchPath": "MAIN",  "type": "DELTA"\n}' | grep -oP 'Location: \K.*' > location.txt
   
-  delta_location=`head -1 location.txt | tr -d '\r'`
-  echo "Recovering delta from $delta_location"
-  wget --load-cookies cookies.txt ${delta_location}/archive -O ${deltaArchiveFile}
+	delta_location=`head -1 location.txt | tr -d '\r'`
+	echo "Recovering delta from $delta_location"
+	wget --load-cookies cookies.txt ${delta_location}/archive -O ${deltaArchiveFile}
 }
 
 downloadPreviousRelease() {
 	if [ -f "${previousRelease}" ]; then
 		echo "Previous published release ${previousRelease} already present"
 	else
-	  echo "Downloading previous published release: ${previousRelease} from S3 ${s3BucketLocation}"
-	  aws s3 cp s3://${s3BucketLocation}${previousRelease} ./
+		echo "Downloading previous published release: ${previousRelease} from S3 ${s3BucketLocation}"
+		aws s3 cp s3://${s3BucketLocation}${previousRelease} ./
 	fi
 }
 
 uploadInputFiles() {
+	filesUploaded=0
+	uploadUrl="${release_url}/api/v1/centers/${releaseCenter}/products/${productKey}/inputfiles"
+	echo "Uploading input files from ${converted_file_location} to ${uploadUrl}"
 
-		filesUploaded=0
-		uploadUrl="${release_url}/api/v1/centers/${releaseCenter}/products/${productKey}/inputfiles"
-		echo "Uploading input files from ${converted_file_location} to ${uploadUrl}"
-
-		for file in `find . -type f -path "./${converted_file_location}/*" -name '*.txt'`;
-		do
-			echo "Upload Input File ${file}"
-			curl ${commonParams} -F "file=@${file}" ${release_url}/api/v1/centers/${releaseCenter}/products/${productKey}/inputfiles | grep HTTP | ensureCorrectResponse
-			filesUploaded=$((filesUploaded+1))
-		done
-		
-		if [ ${filesUploaded} -lt 1 ] 
-		then
-			echo -e "Failed to find files to upload.\nScript halted."
-			exit -1
-		fi
+	for file in `find . -type f -path "./${converted_file_location}/*" -name '*.txt'`;
+	do
+		echo "Upload Input File ${file}"
+		curl ${commonParams} -F "file=@${file}" ${release_url}/api/v1/centers/${releaseCenter}/products/${productKey}/inputfiles | grep HTTP | ensureCorrectResponse
+		filesUploaded=$((filesUploaded+1))
+	done
+	
+	if [ ${filesUploaded} -lt 1 ] 
+	then
+		echo -e "Failed to find files to upload.\nScript halted."
+		exit -1
+	fi
 }
 
 callSrs() {
-
 	echo "Deleting previous delta Input Files "
 	curl ${commonParams} -X DELETE ${release_url}/api/v1/centers/${releaseCenter}/products/${productKey}/inputfiles/*.txt | grep HTTP | ensureCorrectResponse
 	
